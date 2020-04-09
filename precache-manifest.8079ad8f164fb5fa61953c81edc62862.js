9f8066bac6874b5b37e85db74d1cc47675487685
Wipawe Sirikolkarn <wsirikolkarn@gmail.com>
2020-04-09 00:59:34 -0400

    Updates

---
+++ precache-manifest.8079ad8f164fb5fa61953c81edc62862.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8476b37e62bc56f2bf64244a74e2b150",
+    "url": "/pelly/index.html"
+  },
+  {
+    "revision": "c1d0ed4768f78f6867e0",
+    "url": "/pelly/static/css/main.f3eefa68.chunk.css"
+  },
+  {
+    "revision": "f372d87476e7f81461e4",
+    "url": "/pelly/static/js/2.c34ff89f.chunk.js"
+  },
+  {
+    "revision": "492d7e05a4c2e81da7932422a4d31130",
+    "url": "/pelly/static/js/2.c34ff89f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d0ed4768f78f6867e0",
+    "url": "/pelly/static/js/main.971851e5.chunk.js"
+  },
+  {
+    "revision": "788d9aaf7ef7f6ad7d8e",
+    "url": "/pelly/static/js/runtime-main.0d646a73.js"
+  },
+  {
+    "revision": "1918ed983150cec7145170e373669cfb",
+    "url": "/pelly/static/media/PelotareSans-400wt.1918ed98.otf"
+  },
+  {
+    "revision": "a05829cd215adc1936fc51d96c770c84",
+    "url": "/pelly/static/media/PelotareSans-700wt.a05829cd.otf"
+  },
+  {
+    "revision": "9149264978586e6e5af580be3ea35c20",
+    "url": "/pelly/static/media/logo-top.91492649.png"
+  },
+  {
+    "revision": "5fd3c4f04b7a7d3a3c5bd9d68b3a866d",
+    "url": "/pelly/static/media/logo_init.5fd3c4f0.png"
+  },
+  {
+    "revision": "9b99f013717c93f45fcba3187f74df0a",
+    "url": "/pelly/static/media/plate.9b99f013.png"
+  }
+]);
